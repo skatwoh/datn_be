8bc615de7e32ccc58714e6a75f9b73174a2af089
khanhdq <khanhdq2411@gmail.com>
2023-09-16 15:00:43 +0700

    Thêm swagger

--- src/main/java/be/bds/bdsbes/config/SwaggerConfig.java
+++ src/main/java/be/bds/bdsbes/config/SwaggerConfig.java
@@ -1,29 +1,44 @@
 package be.bds.bdsbes.config;
 
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    @Value("${jwt.header}")
+    private String tokenHeader;
+
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("be.bds.bdsbes.resource"))
+                .paths(PathSelectors.any())
                 .build()
+                .securitySchemes(Arrays.asList(apiKey()))
+                .securityContexts(Arrays.asList(securityContext()))
                 .apiInfo(metaData());
     }
+
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
                 .title("Hotel Manager")
@@ -32,6 +47,25 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .contact(new Contact("Dao Quoc Khanh", "https://github.com/skatwoh", "khanhdq2411@gmail.com"))
                 .build();
     }
+
+    private springfox.documentation.spi.service.contexts.SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/.*")) // Thay đổi đường dẫn dựa trên yêu cầu của bạn
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        final AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{authorizationScope};
+        return Arrays.asList(new SecurityReference("BearerToken", authorizationScopes));
+    }
+
+    private springfox.documentation.service.ApiKey apiKey() {
+        return new springfox.documentation.service.ApiKey("BearerToken", tokenHeader, "header");
+    }
+
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
@@ -41,5 +75,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
+
+
 
 
