a4eaa910e2517abf4e46b4fcae0ddf870b586edc
khanhdq <khanhdq2411@gmail.com>
2024-01-10 20:54:19 +0700

    update info customer

--- src/main/java/be/bds/bdsbes/service/iService/IKhachHangService.java
+++ src/main/java/be/bds/bdsbes/service/iService/IKhachHangService.java
@@ -26,4 +26,6 @@ public interface IKhachHangService {
     Boolean createOrUpdate(KhachHangDTO khachHangDTO) throws ServiceException;
 
     Long findIdByCCCD(String cccd);
+
+    KhachHang updateKH(KhachHangDTO khachHangDTO, Long id);
 }
